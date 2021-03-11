1d220c970505d7c573cef0d65a0942103a26e43a
Soner ATEŞ <captain_REX_camino@hotmail.com>
2021-03-11 18:06:39 +0300

    Add project files.

---
+++ Constructors/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                        //KULLANIM ŞEKİLLERİ
+                        //1. Kullanım
+            Customer customer1 = new Customer {Id=1, FirstName="Soner", LastName="ATEŞ", City="Antalya" };
+                        //2. Kullanım
+            Customer customer2 = new Customer ( 2, "Murat", "ATICI", "İstanbul" );
+            Console.WriteLine("ID :" +customer2.Id);
+            Console.WriteLine("İSİM :" +customer2.FirstName);
+            Console.WriteLine("SOYAD :" + customer2.LastName);
+            Console.WriteLine("ŞEHİR :" + customer2.City);
+
+
+        }
+    }
+    class Customer
+    {
+                    //2. Kullanım Parametresi
+        public Customer(int id, string firstName, string lastName, string city)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+                    //1. Kullanım Parametresi
+        //default Constructor
+        public Customer()
+        {
+           
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sehirler = new List<string>();
+            sehirler.Add("Muğla");
+            sehirler.Add("Antalya");
+            sehirler.Add("İstanbul");
+            sehirler.Add("Bursa");
+            sehirler.Add("Adana");
+            sehirler.Add("Malatya");
+            Console.WriteLine(sehirler.Count);
+
+            MyList<string> sehirler2 = new MyList<string>();
+            sehirler2.Add("Ankara");
+            sehirler2.Add("İstanbul");
+            sehirler2.Add("Antalya");
+            sehirler2.Add("Bursa");
+            sehirler2.Add("İzmir");
+            sehirler2.Add("Adana");
+            sehirler2.Add("Adıyaman");
+            Console.WriteLine(sehirler2.Count);
+
+            MyList<int> sayilar = new MyList<int>();
+            sayilar.Add(31);
+        }
+    }
+    class MyList<T> //Generic Class
+    {
+        T[] _array;
+        T[] _tempArray;
+        public MyList()
+        {
+            _array = new T[0];
+        }
+        public void Add(T item)
+        {
+            _tempArray = _array;
+            _array = new T[_array.Length + 1];
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+            }
+            _array[_array.Length - 1] = item;
+        }
+
+        public int Count
+        {
+            get { return _array.Length; }
+        }
+
+    }
+}
